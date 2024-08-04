593c2a283a70553fee02812de5c6b790511c76b0
zahra mortazavi <zahramortazavi23@gmail.com>
2024-08-04 11:58:52 +0330

    add vercel.json

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const NotFoundHandler = require("./src/common/exception/not-found.handler");
 const AllExceptionHandler = require("./src/common/exception/all-exception.handler");
 const cors = require("cors");
 dotenv.config();
-async function main () {
+async function main() {
     const app = express();
     const port = process.env.PORT;
     require("./src/config/mongoose.config");
@@ -14,7 +14,7 @@ async function main () {
         origin: "*"
     }));
     app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.static("public"));
     app.use(mainRouter);
     SwaggerConfig(app);
@@ -24,4 +24,6 @@ async function main () {
         console.log(`server: http://localhost:${port}`);
     });
 }
+module.exports = app;
 main();
+
--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,6 @@
 const CookieNames = require("../../common/constant/cookie.enum");
 const NodeEnv = require("../../common/constant/env.enum");
-const {AuthMessage} = require("./auth.messages");
+const { AuthMessage } = require("./auth.messages");
 const authService = require("./auth.service");
 const autoBind = require("auto-bind");
 class AuthController {
@@ -9,9 +9,9 @@ class AuthController {
         autoBind(this);
         this.#service = authService;
     }
-    async sendOTP (req, res, next) {
+    async sendOTP(req, res, next) {
         try {
-            const {mobile} = req.body;
+            const { mobile } = req.body;
             await this.#service.sendOTP(mobile);
             return res.json({
                 message: AuthMessage.SendOtpSuccessfully
@@ -20,10 +20,10 @@ class AuthController {
             next(error);
         }
     }
-    async checkOTP (req, res, next) {
+    async checkOTP(req, res, next) {
         try {
-            const {mobile, code} = req.body;
-            const {accessToken, refreshToken} = await this.#service.checkOTP(mobile, code);
+            const { mobile, code } = req.body;
+            const { accessToken, refreshToken } = await this.#service.checkOTP(mobile, code);
             return res.status(200).json({
                 message: AuthMessage.LoginSuccessfully,
                 accessToken,
@@ -33,10 +33,10 @@ class AuthController {
             next(error);
         }
     }
-    async checkRefreshToken (req, res, next) {
+    async checkRefreshToken(req, res, next) {
         try {
-            const {refreshToken: token} = req.body;
-            const {accessToken, refreshToken} = await this.#service.checkRefreshToken(token);
+            const { refreshToken: token } = req.body;
+            const { accessToken, refreshToken } = await this.#service.checkRefreshToken(token);
             return res.status(200).json({
                 message: AuthMessage.LoginSuccessfully,
                 accessToken,
@@ -46,7 +46,7 @@ class AuthController {
             next(error);
         }
     }
-    async logout (req, res, next) {
+    async logout(req, res, next) {
         try {
             await this.#service.clearToken(req.user._id);
             return res.json({
